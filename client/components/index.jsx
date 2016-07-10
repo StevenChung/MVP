8b072da1d22761f0a496d1970040f85bf9d4ad75
Steven Chung <steven.chung1@gmail.com>
2016-07-10 14:19:08 -0700

    Playing with Webpack for Client-side

---
+++ client/components/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const Greetings = () => <h1>Hello, world!</h1>;
+
+ReactDOM.render(<Greetings />, document.querySelector('.container'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: __dirname + '/client/components/',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: 'node_modules',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+};
