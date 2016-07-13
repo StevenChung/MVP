2b0c1cfeaab84010d75e11323b4179bf929f2fd2
Steven Chung <steven.chung1@gmail.com>
2016-07-12 17:38:17 -0700

    one hour db

--- client/components/PokemonMarker.jsx
+++ client/components/PokemonMarker.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
 export default ({ url }) => {
-  // var randInt = Math.floor(Math.random() * 151);
   return (
     <div className="pokemonMarker">
       <img src={url} />
